5e4cf2a36324a4b7e8d992d764522cbdcf589dda
Steve Tang <stevetang92@gmail.com>
2019-06-28 16:29:11 -0700

    add input command to my spyder

--- how-debug-works/my-spyder.py
+++ how-debug-works/my-spyder.py
@@ -1,6 +1,4 @@
-impoirt sys
-
-
+import sys
 
 def remove_html_markup(s):
     tag = False
@@ -20,29 +18,45 @@ def remove_html_markup(s):
     return out
 
 stepping = True
-breakpoints = {}
+breakpoints = {9:True, 14:True}
 
-def debug(command, my_arg, my_locals):
+def debug(command, arg, my_locals):
     global stepping
     global breakpoints
     if command.find(' ') > 0:
         arg = command.split(' ')[1]
     else:
         arg = None
-    if command.startwith('s':)
 
+    if command.startwith('s'):
+        stepping = True
+        return True
+    elif command.startwith('c'):
+        stepping = False
+        return True
+    elif command.startwith('q'):
+        sys.exit(0)
+    else:
+        print ('No such command', repr(command))
+commands = ['s', 's', 'q']
+
+def input_command():
+    global commands
+    return commands.pop(0)
 
 def traceit(frame, event, arg):
     global stepping
     global breakpoints
 
     if event == "Line":
-        if stepping or breakpoints.has_key(frame.f_line):
-            print(event, frame.f_lineno, frame.f_code.co_name, frame.f_locals)
-            command = input(command)
-            resume = debug(command, arg, frame.f_locals)
+        if stepping or frame.f_line in breakpoints:
+            resume = False
+            while not resume:
+                print(event, frame.f_lineno, frame.f_code.co_name, frame.f_locals)
+                command = input_command()
+                resume = debug(command, arg, frame.f_locals)
     return traceit
 
-sys,settrace(traceit)
+sys.settrace(traceit)
 
-sys,settrace(None)
+sys.settrace(None)
